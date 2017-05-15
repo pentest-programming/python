fd7711c2ef4d361a4e809964f3099713d233760f
Pentesting-Programming <pentest.programming@gmail.com>
2017-05-15 22:34:18 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,55 @@
+# Dats Structure
+  - int
+  - string
+  - float
+  - array
+  - dict
+  
+# Important Functions
+  - id
+  - type
+  - len
+  - str
+  - int
+  - enumerate
+  
+  
+# Loops
+  - For
+  - While
+  
+# If Else 
+
+# Array
+
+# Dict
+
+# Functions
+
+# Classess
+
+# Modules
+  - os
+  - subprocess
+  - argparse
+  - logging
+  - re
+
+
+# try - catch, exceptions
+
+# Pythonic 
+  
+# Multithread
+
+# Http
+
+# Dns
+
+# WebService
+
+
+
+
+
+
